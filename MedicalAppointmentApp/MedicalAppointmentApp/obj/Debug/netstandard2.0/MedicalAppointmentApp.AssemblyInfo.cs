f240119ea259324370db183563cf887cede12e8c
Derty990 <kluczbart@gmail.com>
2025-04-09 20:14:39 +0200

    commit

--- MedicalAppointmentApp/MedicalAppointmentApp/obj/Debug/netstandard2.0/MedicalAppointmentApp.AssemblyInfo.cs
+++ MedicalAppointmentApp/MedicalAppointmentApp/obj/Debug/netstandard2.0/MedicalAppointmentApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalAppointmentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92030e2d3964c669b2d92fb527a7914d56594af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed4374fae2d955a14e855d61ae75683b547b465")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalAppointmentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalAppointmentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
